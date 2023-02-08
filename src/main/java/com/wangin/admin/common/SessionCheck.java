411c21a9dd672d58d215f5c596d1bcd7a379fda6
최윤석 <106067627+jellikk@users.noreply.github.com>
2023-02-08 16:34:35 +0900

    Revert "yunseok_login(23/02/08)"
    This reverts commit dcabfddc8c69a5a6dd42258457e0cd6206f43aba.

--- src/main/java/com/wangin/admin/common/SessionCheck.java
+++ src/main/java/com/wangin/admin/common/SessionCheck.java
@@ -1,15 +1,4 @@
 package com.wangin.admin.common;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 public class SessionCheck {
-    public boolean loginSessionCheck(HttpServletRequest request){
-        HttpSession session = request.getSession();
-        if(session.getAttribute("user_signature")==null){
-            return false;
-        }else {
-            return true;
-        }
-    }
 }
